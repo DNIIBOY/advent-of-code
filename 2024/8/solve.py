fa542d0ae48fe808f395cb923140531d7ac0192c
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2024-12-12 10:30:56 +0100

    Fucked 8

---
+++ 2024/8/solve.py
@@ -0,0 +1,38 @@
+import pyperclip
+from time import perf_counter
+
+
+def display_output(func) -> None:
+    with open("input.txt", "r", encoding="utf-8") as f:
+        values = f.read().splitlines()
+    values = [
+        "MMMSXXMASM",
+        "MSAMXMSMSA",
+        "AMXSXMAAMM",
+        "MSAMASMSMX",
+        "XMASAMXAMM",
+        "XXAMMXXAMA",
+        "SMSMSASXSS",
+        "SAXAMASAAA",
+        "MAMMMXMMMM",
+        "MXMXAXMASX",
+    ]
+
+    start = perf_counter()
+    result = func(values)
+    end = perf_counter()
+    print(f"--- Got result in {end-start:.2f}s---")
+    pyperclip.copy(result)
+    print(result)
+    print("--- Copied to clipboard ---")
+
+
+def main(values: list) -> None:
+    for row in values:
+        for char in row:
+            if char == "X":
+                pass
+
+
+if __name__ == "__main__":
+    display_output(main)
